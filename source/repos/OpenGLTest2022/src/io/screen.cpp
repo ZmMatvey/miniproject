1470a682c3d444483d495a6a4960763c3abbd691
ZmMatvey <115418268+ZmMatvey@users.noreply.github.com>
2023-01-10 21:16:46 +0300

    Add files via upload

--- source/repos/OpenGLTest2022/src/io/screen.cpp
+++ source/repos/OpenGLTest2022/src/io/screen.cpp
@@ -3,20 +3,27 @@
 #include "keyboard.h"
 #include "mouse.h"
 
-unsigned int screen::SCR_WIDTH = 800; 
-unsigned int screen::SCR_HEIGHT = 600;
+int screen::SCR_WIDTH = 0;
+int screen::SCR_HEIGHT = 0;
+bool screen::init_context_window = false;
 
-void screen::framebufferSizeCallback(GLFWwindow* widnow, int width, int height) {
+void screen::framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+	//делает окно контекстным
+	glfwMakeContextCurrent(window);
+
+	//задаёт начальную позицию относительно контекстного окна и размеры framebuffer
 	glViewport(0, 0, width, height);
 
-	SCR_WIDTH = width;
-	SCR_HEIGHT = height;
+	screen::SCR_WIDTH = width;
+	screen::SCR_HEIGHT = height;
 }
-void screen::OpenGL_version() {
-	//OpenGL version 3.3
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
+void screen::OpenGL_Version(int major, int minor) {
+	//OpenGL версии 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, major);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, minor);
+	
+	//получение пакета функций до версии 3.3
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); 
 
 	//для запуска на Mac OS
@@ -25,50 +32,117 @@ void screen::OpenGL_version() {
 #endif //__APPLE__
 }
 
-screen::screen()
-	: window(nullptr) {}
-
-bool screen::init() {
-	window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL Project", NULL, NULL);
-	if (window == NULL) {
-		// window not created;
-		return false;
+void screen::Init_Glad() {
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		glfwTerminate();
+		throw ("Failed to initialize GLAD");
 	}
+}
+
+screen::screen()
+	: window(NULL) {}
+
+void screen::init(int widht, int height, const char* name) {
+	//создаёт окно соответствующего размера с названием OpenGL Project, 
+	//4 параметр принимает указатель на монитор, 5 - частота обновлений кадров монитора
+	window = glfwCreateWindow(widht, height, name , NULL, NULL);
+
+	if (window == NULL) {
+		glfwTerminate();
+		throw "Could not open window";
+		return;
+	}
+	if (!init_context_window) {
+		glfwMakeContextCurrent(window);
+		init_context_window = true;
+	}
+}
+
+void screen::MakeContextCurrent() {
+	//делает окно контекстным
 	glfwMakeContextCurrent(window);
 
-	return true;
+	int width, height;
+	glfwGetWindowSize(window, &width, &height);
+	framebufferSizeCallback(window, width, height);
+}
+
+GLFWwindow* screen::getWindow() const {
+	return window;
+}
+
+int screen::getWidth() {
+	return screen::SCR_WIDTH;
+}
+
+int screen::getHeight() {
+	return screen::SCR_HEIGHT;
+}
+
+void screen::setFramebufferSize() const {
+	//вызывает вспомогательную функцию от текущих параметров окна, при изменении размера окна
+	glfwSetFramebufferSizeCallback(window, screen::framebufferSizeCallback);
+}
+
+void screen::setKeyboard() const {
+	//вызывает вспомогательную функцию от текущих параметров ввода ключей с клавиатуры в окно 
+	glfwSetKeyCallback(window, keyboard::keyCallback);
+}
+
+void screen::setMouse(int mode_cursor) const {
+	//модификаторы курсора
+	glfwSetInputMode(window, GLFW_CURSOR, mode_cursor);
+
+	//аналогично клавиатуре
+	glfwSetCursorPosCallback(window, mouse::cursorPosCallback); 
+	glfwSetMouseButtonCallback(window, mouse::mouseButtonCallback);
+	glfwSetScrollCallback(window, mouse::mouseWheelCallback);
 }
 
 void screen::setParameters() const {
-	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+	setFramebufferSize();
+	setKeyboard();
+	setMouse();
 
-	glfwSetFramebufferSizeCallback(window, screen::framebufferSizeCallback);
-
-	glfwSetKeyCallback(window, keyboard::keyCallback);
-
-	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-	glfwSetCursorPosCallback(window, mouse::cursorPosCallback);
-	glfwSetMouseButtonCallback(window, mouse::mouseButtonCallback);
-	glfwSetScrollCallback(window, mouse::mouseWheelCallback);
-
-	glEnable(GL_BLEND);
+	screen::setEnable();
 }
 
-// main loop
+void screen::setEnable() {
+	glEnable(GL_DEPTH_TEST); //тест глубины
+}
+
+void screen::setSize(int widht, int height) const {
+	glfwSetWindowSize(window, widht, height);
+}
+
+void screen::setPos(int pos_x, int pos_y) const {
+	glfwSetWindowPos(window, pos_x, pos_y);
+}
+
 void screen::update() const {
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	//очистка окна от того, что было нарисовано в предыдущем кадре 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
 void screen::newFrame() const {
-	glfwSwapBuffers(window);
+	//один(никакой многопоточности для рисования!!!) буффер рисует, другой выводит на экран, чтобы не было мерцаний при смене картинки
+	glfwSwapBuffers(window); 
 }
 
-// window closing
 bool screen::shouldClose() const {
 	return glfwWindowShouldClose(window);
 }
 
 void screen::setShouldClose(bool shouldClose) const {
 	glfwSetWindowShouldClose(window, shouldClose);
+}
+
+void screen::setFullScreen(int mode) const {
+	glfwMaximizeWindow(window);
+	glfwSetInputMode(window, GLFW_CURSOR, mode);
+}
+
+void screen::setRestoreScreen(int mode) const {
+	glfwRestoreWindow(window);
+	glfwSetInputMode(window, GLFW_CURSOR, mode);
 }
--- source/repos/OpenGLTest2022/src/io/screen.h
+++ source/repos/OpenGLTest2022/src/io/screen.h
@@ -1,27 +1,46 @@
 #ifndef SCREEN_H
 #define SCREEN_H
 
+#define GLFW_INCLUDE_NONE
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 class screen final
 {
 private:
-	GLFWwindow* window;
+	GLFWwindow* window; 
 
 	static void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 
-public:
-	static unsigned int SCR_WIDTH;
-	static unsigned int SCR_HEIGHT;
+	//размеры контекстного окна
+	static int SCR_WIDTH;
+	static int SCR_HEIGHT;
 
-	static void OpenGL_version();
+	static bool init_context_window;
+
+public:
+	static void OpenGL_Version(int major = 3, int minor = 3); 
+	static void Init_Glad();
 
 	screen();
 
-	bool init();
+	void init(int widht = 800, int height = 600, const char* name = "OpenGL Project");
 
+	void MakeContextCurrent();
+
+	GLFWwindow* getWindow() const;
+	static int getWidth();
+	static int getHeight();
+
+	void setFramebufferSize() const;
+	void setKeyboard() const;
+	void setMouse(int mode_cursor = GLFW_CURSOR_NORMAL) const;
 	void setParameters() const;
+
+	static void setEnable();
+
+	void setSize(int widht, int height) const;
+	void setPos(int pos_x, int pos_y) const;
 
 	// main loop
 	void update() const; 
@@ -29,8 +48,10 @@ public:
 
 	// window closing
 	bool shouldClose() const;
-	void setShouldClose(bool shouldClose) const;
 
+	void setShouldClose(bool shouldClose) const;
+	void setFullScreen(int mode = GLFW_CURSOR_DISABLED) const;
+	void setRestoreScreen(int mode = GLFW_CURSOR_NORMAL) const;
 };
 
 #endif //SCREEN_H
